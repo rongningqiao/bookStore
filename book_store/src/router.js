d53dfb8ff31de48ee2eb5ea6044e7181e1d30c55
rongningqiao@gmail.com <rongningqiao@gmail.com>
2019-12-24 21:57:49 +0800

    -m第一次提交

---
+++ book_store/src/router.js
@@ -0,0 +1,12 @@
+import Vue  from 'vue'
+import VueRouter from "vue-router";
+Vue.use(VueRouter)
+var router=new VueRouter({
+    routes:[
+        {
+            // path:'',
+            // component:
+        },
+    ]
+})
+export default router;
